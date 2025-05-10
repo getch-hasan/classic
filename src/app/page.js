80e40808bffa60bbd0f677d8e01fa77f38f88618
getch-hasan <hasan15-3112@diu.edu.bd>
2025-05-11 04:35:07 +0600

    modal open

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client";
 import Banner from "@/components/hero/banner";
 import CategoryMenu from "@/components/hero/category";
+import RamModal from "@/components/productModal";
 import ProductCard from "@/components/singleCard";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
 const [products, setProducts] = useState([]);
+console.log(products);
   const fetchProducts = useCallback(async () => {
     try {
       const res = await fetch("/products.json");
@@ -22,6 +24,16 @@ const [products, setProducts] = useState([]);
   useEffect(() => {
     fetchProducts();
   }, []);
+
+ const [modalOpen, setModalOpen] = useState(false);
+ const [selectedProduct, setSelectedProduct] = useState(null);
+ const [qty, setQty] = useState(1);
+ console.log(modalOpen) 
+const handleModal = (product) => {
+  setSelectedProduct(product); // Set selected product
+  setModalOpen(true);          // Then open the modal
+};
+
   return (
 <div className="container-custom">
   <div className="flex">
@@ -36,16 +48,31 @@ const [products, setProducts] = useState([]);
 
  <section className="py-10 px-4 max-w-7xl mx-auto">
       <div className="text-center mb-8">
-        <p className="text-red-500 font-medium">Our Products</p>
-        <h2 className="text-2xl font-bold">Explore Our Products</h2>
+<p className="text-red-500 font-semibold border-l-[20px]  border-[#DB4444] pl-2 inline-block ">Our Products</p>
+
+
+        <h2 className="text-4xl font-semibold ">Explore Our Products</h2>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products?.map((product, index) => (
-          <ProductCard key={index} {...product} />
+          <ProductCard key={index} product={product} handleModal={handleModal}  />
         ))}
       </div>
     </section>
+
+
+  {modalOpen && (
+<RamModal
+  isOpen={modalOpen}
+  onClose={() => setModalOpen(false)}
+  product={selectedProduct}
+  setQty={setQty}
+  qty={qty}
+/>
+
+)}
+
 </div>
 
 
--- src/components/singleCard/index.jsx
+++ src/components/singleCard/index.jsx
@@ -1,44 +1,71 @@
 import Image from "next/image";
+import { useState } from "react";
 import { FaStar, FaRegHeart, FaEye } from "react-icons/fa";
 
-const ProductCard = () => {
+const ProductCard = ({product ,handleModal }) => {
+    const [selectedColor, setSelectedColor] = useState("");
+    
   return (
-    <div className="w-64  rounded  relative group">
-      {/* Wishlist and View Icons */}
-      <div className="absolute top-3 right-3 flex flex-col gap-2">
+    <div className="w-64  rounded">
+      {/* Product Image */}
+      <div className="relative group  ">
+        <Image height={250} width={270}
+          src={product?.mainImage}
+          alt="ASUS FHD Gaming Laptop"
+          className="w-full h-[250px] bg-gray-100 p-8 object-contain rounded-lg "
+        />
+          <div className="absolute top-3 right-3 flex flex-col gap-2">
         <button className="w-6 h-6 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100">
           <FaRegHeart className="text-gray-600" />
         </button>
-        <button className="w-6 h-6 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100">
-         <Image src='/product/eye.svg' height={20} width={17} alt=""/>
-        </button>
-      </div>
+        <button onClick={() => handleModal(product)} className="w-6 h-6 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100">
+  <Image src={'/product/eye.svg'} height={20} width={17} alt=""/>
+</button>
 
-      {/* Product Image */}
-      <div className=" ">
-        <Image height={250} width={270}
-          src="/product/car.png"
-          alt="ASUS FHD Gaming Laptop"
-          className="w-full bg-gray-100 p-8 object-contain rounded-lg "
-        />
+
+          
+      </div>
+      <p className="absolute inline-block bg-[#00FF66] rounded px-2 top-3 left-3 text-white ">{product?.arrival}</p>
+    <button className="absolute  hidden group-hover:block  bottom-0 w-full bg-black font-medium py-1 text-white"> Add To Card</button>
       </div>
 
    <div className="py-4">
        {/* Product Title */}
       <h3 className="text-base leading-6 font-medium text-gray-800 mb-1">
-        ASUS FHD Gaming Laptop
+       {product?.name}
       </h3>
 
       {/* Price and Rating */}
       <div className="flex items-center gap-2">
-        <span className="text-[#DB4444] font-medium">$700</span>
+        <span className="text-[#DB4444] font-medium">${product?.price}</span>
         <div className="flex items-center  text-yellow-400 text-sm">
           {[...Array(5)].map((_, i) => (
             <FaStar key={i} />
           ))}
         </div>
-        <span className="text-gray-500 text-sm font-semibold">(325)</span>
+        <span className="text-gray-500 text-sm font-semibold">({product?.rating?.stars})</span>
       </div>
+  <div className="flex items-center gap-2 mt-2">
+  {product?.colours?.map((color, index) => (
+    <label key={index} htmlFor={`color-${index}`} className="cursor-pointer">
+      <input
+        type="radio"
+        name="color"
+        value={color?.hex}
+        id={`color-${index}`}
+        className="hidden" 
+      />
+      <div
+        className={`w-5 h-5 rounded-full transition border-2 ${
+color?.selected ? 'border-black' : 'border-transparent'
+        }`}
+        style={{ backgroundColor: color.hex }}
+      />
+    </label>
+  ))}
+</div>
+
+
    </div>
     </div>
   );
